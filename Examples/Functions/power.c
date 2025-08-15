cf4e534dc5a70811ee19f6e72c636df7f245df16
switchcopa <100treyholds@gmail.com>
2025-08-15 21:23:40 +0100

    feat: completed power function implementation

--- Examples/Functions/power.c
+++ Examples/Functions/power.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 
+int power(int base, int n) {
+    int p; 
+    
+    p = 1;
+    for (int i = 0; i <= n; i++) {
+        p = p * base; 
+    }
 
+    return p;
+}
 
 int main() {
-    
+    printf("2 to the power of 3 is: %d\n", power(2,3));
+    printf("-3 to the power of 2 is: %d\n", power(-3, 2));
+
     return 0;
 }
