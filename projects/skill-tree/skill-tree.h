5e26680b9e59ad3d95bbe0689591a0e99f20deb3
switchcopa <100treyholds@gmail.com>
2025-08-20 23:18:11 +0100

    feat: add new skill tree project with file header function declarations

---
+++ projects/skill-tree/skill-tree.h
@@ -0,0 +1,27 @@
+#ifndef SKILL_TREE_H
+
+#define SKILL_TREE_H
+
+#include <stdio.h>
+#include <stdbool.h>
+
+struct Node* create_node(struct Node* left, struct Node* right, struct Node* parent, const char* name, const char* description, int cost, bool purchased); 
+
+struct Node* init_agility_tree();
+
+struct Node* init_power_tree();
+
+struct Node* init_survivor_tree();
+
+const char** show_purchased_skills(struct Node* skill_tree);
+
+const char** show_skill_tree(struct Node* skill_tree);
+
+bool can_purchase_skill(struct Node* skill_tree, const char* skill);
+
+bool purchase_skill(struct Node* skill_tree, const char* skill); 
+
+bool sell_skill(struct Node* skill_tree, const char* skill);
+
+
+#endif // SKILL_TREE_H
