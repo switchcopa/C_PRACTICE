26bbacd133a5d97c2fb327f8efb594c6378b7667
switchcopa <100treyholds@gmail.com>
2025-09-05 13:08:00 +0100

    add ASCII to integer conversion example

---
+++ Examples/type-conversions/atoi.c
@@ -0,0 +1,11 @@
+#include <stdio.h> 
+
+int atoi(char s[]) {
+    int i, res;
+
+    res = 0;
+    for (i = 0; s[i] >= '0' && s[i] <= '9'; i++) 
+        res = 10 * res + (s[i] - '0');
+    
+    return res;
+}
