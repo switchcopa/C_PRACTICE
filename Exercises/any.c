f3361ab70c0601534f986f2e91fc7be678f975c8
switchcopa <100treyholds@gmail.com>
2025-09-07 12:25:18 +0100

    add any.c exercise.2-5 K&R book

---
+++ Exercises/any.c
@@ -0,0 +1,28 @@
+#include <stdio.h> 
+#include <stdbool.h>
+
+int any(char s1[], char s2[]);
+
+int main(void) {
+    char s1[] = "hello";
+    char s2[] = "world";
+
+    int i = any(s1, s2);
+    printf("%d\n", i);    
+
+    return 0;
+}
+
+int any(char s1[], char s2[]) {
+    int i; 
+    bool seen[256] = {false};
+
+    for (i = 0; s2[i] != '\0'; i++) 
+        seen[(unsigned char)s2[i]] = true;
+
+    for (i = 0; s1[i] != '\0'; i++) 
+        if (seen[(unsigned char)s1[i]]) 
+            return i;
+    
+    return -1;
+}
