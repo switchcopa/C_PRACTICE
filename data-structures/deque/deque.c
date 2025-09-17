344a75ccb6caea561e7429a0cd67e46f9dd39abc
switchcopa <100treyholds@gmail.com>
2025-09-17 17:32:41 +0100

    add deque data structure API

---
+++ data-structures/deque/deque.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "deque.h"
+
+deque* create_deque(size_t size) {
+        deque* dq = malloc(sizeof(deque));
+        if (!dq) return NULL;
+
+        dq->buffer = malloc(sizeof(uint8_t) * size);
+        if (!dq->buffer) return NULL;
+
+        dq->capacity = size;
+        dq->size = 0;
+        dq->front = dq->rear = -1;
+
+        return dq;
+}
+
+bool is_empty(deque* dq) {
+        if (!dq) return false;
+        
+        return dq->size == dq->capacity;
+}
+
+bool is_full(deque* dq) {
+        if (!dq) return false;
+        
+        return ((rear + 1) % capacity == front);
+}
+
+void push_front(deque* dq, uint8_t data) {
+        if (!dq || is_full(dq)) return;
+        
+        dq->front = (front - 1 + capacity) % capacity; 
+        dq->buffer[dq->front] = data;
+        dq->size++;
+}
+
+void push_back(deque* dq, uint8_t data) {
+        if (!dq || is_full(dq)) return;
+        
+        dq->buffer[dq->rear] = data;
+        dq->rear = (rear + 1) % capacity;
+        dq->size++;
+}
+
+uint8_t pop_front(deque* dq) {
+        if (!dq || is_empty(dq)) return 0;
+        
+        dq->size--;
+        return dq->buffer[dq->front--];
+}
+
+uint8_t pop_back(deque* dq) {
+        if (!dq || is_empty(dq)) return 0;
+        
+        dq->size--;
+        return dq->buffer[dq->rear--];
+}
+
+void print_deque(deque* dq) {
+        if (!dq) {
+                printf("deque is NULL\n");
+                return;
+        }
+
+        else if (is_empty(dq)) {
+                printf("deque is empty\n");
+                return;
+        }
+
+        printf("[ ");
+        for (int i = dq->front; i < dq->size; i = (i+1) % capacity) 
+                printf("%d ", dq->buffer[i]);
+
+        printf("]\n");
+}
---
+++ data-structures/deque/deque.h
@@ -0,0 +1,26 @@
+#ifndef DEQUE_H 
+#define DEQUE_H 
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#define MAX_DEQUE_SIZE 10
+
+typedef struct {
+        uint8_t* buffer;
+        size_t capacity;
+        size_t size;
+        signed int rear;
+        signed int front;
+} deque;
+
+deque* create_deque(size_t size);
+bool is_empty(deque* dq);
+bool is_full(deque* dq);
+void push_front(deque* dq, uint8_t data);
+void push_back(deque* dq, uint8_t data);
+uint8_t pop_front(deque* dq);
+uint8_t pop_back(deque* dq);
+void print_deque(deque* dq);
+
+#endif 
