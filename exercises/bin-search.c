43569edd32bec7bd463e3dab8ab4d3e34b2a5dd5
switchcopa <100treyholds@gmail.com>
2025-09-19 14:24:46 +0100

    solve binary search exercise K&R

---
+++ exercises/bin-search.c
@@ -0,0 +1,23 @@
+/* binsearch: find x in v[0] <= v[1] <= ... <= v[n-1] */
+
+// [0,1,2,3,4,5,6,7]
+
+int binsearch(int x, int v[], int n)
+{
+        int low, high, mid;
+
+        low = 0;
+        high = n - 1;
+        while (low <= high) {
+                mid = (low + high) / 2;
+                if (x < v[mid])
+                        high = mid + 1;
+                else if (x > v[mid])
+                        low = mid + 1;
+                else
+                        return mid;
+        }
+
+
+        return -1;
+}
