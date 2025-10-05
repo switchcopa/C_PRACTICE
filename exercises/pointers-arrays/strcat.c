8ab6692746a37dab0353e1a3cc269b1cd2b08f31
switchcopa <100treyholds@gmail.com>
2025-10-05 15:26:51 +0100

    problem: add solved problem 5-3 strcat K&R book with array and pointers version

---
+++ exercises/pointers-arrays/strcat.c
@@ -0,0 +1,20 @@
+/* str_cat: copies string t to the end of s array version*/
+void str_cat_arr(const char *s, char *t) {
+        int i, j;
+        
+        for (i = 0; t[i] != '\0'; i++)
+                ;
+        for (j = 0; (t[i] = s[j]) != '\0'; i++, j++)
+                ;
+}
+
+/* str_cat: copies string t to the end of s pointers version*/
+void str_cat_ptr(const char *s, char *t) {
+        while (*t != '\0')
+                t++;
+
+        while ((*t = *s) != '\0') {
+                t++;
+                s++;
+        }
+}
