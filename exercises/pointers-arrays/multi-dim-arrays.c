d7b5d6b559b08917e4d8941e829dcbbcf2e4fdf5
switchcopa <100treyholds@gmail.com>
2025-10-11 15:45:52 +0100

    exercise: add multi-dim-arrays.c exercise with error checking exercise 5-8 page 93

---
+++ exercises/pointers-arrays/multi-dim-arrays.c
@@ -0,0 +1,36 @@
+static char daytab[2][13] = {
+        {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
+        {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
+};
+
+/* day_of_year: set day of year from month & day */
+int day_of_year(int year, int month, int day)
+{
+        if ((1 <= month && month <= 12) && (1 <= day && day <= daytab[leap][month])) {
+                int i, leap;
+                leap = year%4 == 0 && year%100 != 0 || year%400 == 0;
+
+                for (i = 1; i < month; i++) 
+                        day += daytab[leap][i];
+                return day;
+        } else
+                return -1;
+}
+
+/* month_day: set month, day from day of year*/
+void month_day(int year, int yearday, int *pmonth, int *pday)
+{
+        int i, leap, maxday;
+        leap = year%4 == 0 && year%100 != 0 || year%400 == 0;
+         
+        maxday = leap ? 366 : 365; 
+        if (yearday < 1 || yearday > maxday) {
+                *pmonth = *pday = -1;
+                return;
+        }
+
+        for (i = 1; yearday > daytab[leap][i]; i++)
+                yearday -= daytab[leap][i];
+        *pmonth = i;
+        *pday = yearday;
+}
