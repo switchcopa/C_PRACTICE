ef7aaa5e3083b77522f831f4903c14cc77d14050
switchcopa <100treyholds@gmail.com>
2025-10-12 12:46:36 +0100

    mini-shell: remove .txt test files and add main.c for build

--- projects/mini-shell/src/main.c
+++ projects/mini-shell/src/main.c
@@ -1,6 +1,40 @@
 #include "mini-shell/mini-shell.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 int main(int argc, char **argv) {
+        if (argc == 1) {
+                printf("Usage: ./test help\n");
+                return 1;
+        }
         
+        if (strcmp(argv[1], "copy") == 0 && argc == 4)
+                copy(argv[2], argv[3]);
+
+        else if (strcmp(argv[1], "delete") == 0 && argc == 3)
+                del_file(argv[2]);
+
+        else if (strcmp(argv[1], "read") == 0 && argc == 3) {
+                char *buf = readf(argv[2]);
+                if (!buf)
+                        return -1;
+
+                printf("%s\n", buf);
+                free(buf);
+        }
+                
+        else if (strcmp(argv[1], "write") == 0 && argc == 4)
+                writef(argv[2], argv[3]);
+
+        else if (strcmp(argv[1], "append") == 0 && argc == 4)
+                appendf(argv[2], argv[3]);
+        
+        else if (strcmp(argv[1], "help") == 0)
+                help();
+        
+        else
+                printf("Unknown arguments or commands\n");
+
         return 0;
 }
