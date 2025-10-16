11c18d8e0ab300fabce06ce069b191b6255646f9
switchcopa <100treyholds@gmail.com>
2025-10-16 18:00:07 +0100

    fix: add parentheses in examples/cl-args/echo-2.c from K&R

--- examples/cl-args/echo-2.c
+++ examples/cl-args/echo-2.c
@@ -4,7 +4,7 @@
 int main(int argc, char **argv) {
 	
 	while (argc-- > 0)
-		printf("%s%s", *++argv, (argc > 1) ? " " : "";
+		printf("%s%s", *++argv, (argc > 1) ? " " : "");
 	
 	printf("\n");
 	return 0;
