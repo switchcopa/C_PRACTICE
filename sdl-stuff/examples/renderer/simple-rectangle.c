48f3b4e008d4ff004b9f8e2a11c4188d82533e53
switchcopa <100treyholds@gmail.com>
2025-10-21 21:45:28 +0100

    sdl: practice with a simple rectangle

---
+++ sdl-stuff/examples/renderer/simple-rectangle.c
@@ -0,0 +1,65 @@
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_events.h>
+#include <SDL2/SDL_render.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define TIME_DIFF(START, END) ((double) (END - START))
+#define TARGET_FPS 60
+#define FRAME_TIME (1000 / TARGET_FPS)
+
+// keep all this here so I memorize the API
+int           SDL_Init(Uint32 flags);
+SDL_Window   *SDL_CreateWindow(const char *title, int x, int y, int w, int h, Uint32 flags);
+SDL_Renderer *SDL_CreateRenderer(SDL_Window *window, int index, Uint32 flags);
+int           SDL_RenderFillRect(SDL_Renderer *renderer, const SDL_Rect *rect);
+int           SDL_PollEvent(SDL_Event *event);
+int           SDL_SetRenderDrawColor(SDL_Renderer *renderer, Uint8 r, Uint8 g, Uint8 b, Uint8 a);
+void          SDL_RenderPresent(SDL_Renderer *renderer);
+void          SDL_DestroyRenderer(SDL_Renderer *renderer);
+
+int main(void) {
+        if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+                fprintf(stderr, "failed to init SDL: %s\n", SDL_GetError());
+                return EXIT_FAILURE;
+        }
+
+        SDL_Window *window = SDL_CreateWindow(
+                "Simple Window",
+                SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                800, 600,
+                SDL_WINDOW_SHOWN
+        );
+
+        SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+        SDL_Event event;
+        static bool running = true;
+        SDL_Rect rect = {
+                350, 250,
+                100, 100
+        };
+        
+        while (running) {
+		Uint32 start_time = SDL_GetTicks();
+                while (SDL_PollEvent(&event))
+                        if (event.type == SDL_QUIT)
+                                running = false;
+
+                SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+                SDL_RenderClear(renderer);
+        
+                SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+                SDL_RenderFillRect(renderer, &rect);
+                SDL_RenderPresent(renderer);
+		
+		Uint32 frame_time = SDL_GetTicks() - start_time;
+		if (frame_time < FRAME_TIME)
+			SDL_Delay(FRAME_TIME - frame_time);
+        }
+
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+
+        return 0;
+}
