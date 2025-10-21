90be041d43fdb4c5d713cbf24bad8b45f7555b01
switchcopa <100treyholds@gmail.com>
2025-10-21 13:33:06 +0100

    final attempt: rewrite tail.c properly

--- exercises/cl-args/tail.c
+++ exercises/cl-args/tail.c
@@ -4,85 +4,102 @@
  */
 
 #include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h>
 #include <string.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <ctype.h>
 
-#define _BUF_SIZE 2056
-#define DEFAULT_LINES 10
+#define _BUF_SIZE 8192U
+#define DEFAULT_LINES 10U
 
-ssize_t get_line(char **restrict lineptr, size_t *restrict n);
+int get_line(char *restrict line, size_t n);
+uint16_t strtoui(const char *restrict start, char **restrict end);
 
-int
-main(int argc, char **argv)
-{
-        int nlines = DEFAULT_LINES;
-        if (argc > 2)
-        {
+int main(int argc, char **argv) {
+	uint16_t n = DEFAULT_LINES;
+	int len;
+
+        if (argc > 2) {
                 printf("Usage: ./tail -n\n");
                 return EXIT_FAILURE;
         }
-        if (argc == 2)
-                nlines = atoi(argv[1]);
 	
-	if (nlines < 0)
-		return EXIT_FAILURE;
-        
-	char **lineptr = malloc(sizeof(char*) * nlines);
-	if (!lineptr)
-		return EXIT_FAILURE;
-
-        char *line = NULL;
-	int i = 0;
-	size_t n = 0, len;
-
-	while ((len = get_line(&line, &n)) >= 0)
-	{
-		if (i >= nlines)
-			i = 0;
-		lineptr[i] = malloc(sizeof(char) * len + 1);
-		if (!lineptr[i])
+	if (argc == 2) {
+		if (argv[1][0] == '-' && isdigit(argv[1][1])) {
+			char *end;
+			n = strtoui(++argv[1], &end);
+                        printf("%u\n", n);
+			if (n == 0)
+				return EXIT_FAILURE;
+                }
+		else  {
+			printf("Usage: ./tail -n\n");
 			return EXIT_FAILURE;
-		strcpy(lineptr[i], line);
+		}
+	}
+       	
+	char *nlineptr[n];
+	static char line[_BUF_SIZE];
+
+	for (int i = 0; i < n; i++)
+		nlineptr[i] = NULL;
+	int i = 0;
+
+	while ((len = get_line(line, _BUF_SIZE)) >= 0) {
+		if (i >= n)
+			i = 0;
+		
+		if (nlineptr[i] != NULL)
+			free(nlineptr[i]);
+		nlineptr[i] = malloc(sizeof(char) * len + 1);
+		if (!nlineptr[i])
+			return EXIT_FAILURE;
+		
+		strcpy(nlineptr[i], line);
+                i++;
 	};
 	
-	i = 0;	
-	while (i < nlines)
-		printf("%s", lineptr[i]);
+	for (int start = i, j = 0; j < n; j++) { 
+		int idx = (start + j) % n;	
+		if (nlineptr[idx]) {
+			printf("%s", nlineptr[idx]);
+			free(nlineptr[idx]);
+		}
+        }
+
         return 0;
 }
 
-ssize_t get_line(char **restrict lineptr, size_t *restrict n)
-{
-        if (lineptr == NULL)
-                return -1;
-
-        if (*lineptr == NULL)
-        {
-                *lineptr = malloc(_BUF_SIZE);
-                if (*lineptr == NULL)
-                {
-                        if (n) *n = 0U;
-                        return -1;
-                }
-
-                if (n) *n = _BUF_SIZE;
-        }
-
-        int c;
-        ssize_t len = 0;
-
-        while (len < (ssize_t)(*n - 1) && (c = getchar()) != EOF) 
-        {
-                (*lineptr)[len++] = c;
-
+int get_line(char *restrict line, size_t n){
+	if (line == NULL)
+		return -1;
+	
+	int len = 0, c;
+	while (len < n - 1 && (c = getchar()) != EOF) {
+		line[len++] = c;
 		if (c == '\n')
 			break;
-        };
+	}
 	
-	if (len == 0 && c == EOF)
+	if (c == EOF && len == 0)
 		return -1;
 
-        (*lineptr)[len] = '\0';
-        return len;
+	line[len] = '\0';
+	return len;
+}
+
+uint16_t strtoui(const char *restrict start, char **restrict end) {
+        if (start == NULL || end == NULL)
+                return 0;
+        while (isspace(*start))
+                start++;
+        
+        uint16_t p = 0;
+	while (isdigit(*start) && *start != '\0') {
+                p = 10 * p + (*start - '0');
+                start++;
+        }
+	
+	*end = (char*)start;	
+        return p;
 }
