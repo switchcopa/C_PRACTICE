0523fd5a69b8456566c504c691f346f2fcbc2278
switchcopa <100treyholds@gmail.com>
2025-10-25 11:09:51 +0100

    exercise: solve exercise 5-14 page 100 K&R book

--- examples/pointers-arrays/extended-sort-input.c
+++ examples/pointers-arrays/extended-sort-input.c
@@ -3,6 +3,7 @@
 #include <string.h>
 
 #define MAXLINES 5000
+#define MAXLEN 1000
 char *lineptr[MAXLINES];
 
 int readlines(char **restrict lineptr, int nlines);
@@ -11,25 +12,27 @@ void writelines(char **restrict lineptr, int nlines);
 void q_sort(void **restrict lineptr, int left, int right, 
 		int (*comp)(void *, void *)); 
 int numcmp(const char *restrict, const char *restrict);
+int reverse_numcmp(const char *restrict, const char *restrict);
 void swap(void **restrict, int, int);
 int get_line(char *restrict line, int maxlines);
 
 int main(int argc, char **argv) {
 	int nlines;
-	int numeric = 0;
+	int numeric = 0, reverse = 0;
 
-	if (argc > 1 && strcmp(argv[1], "-n") == 0)
+	if ((argc > 1 && strcmp(argv[1], "-n") == 0) || (argc > 2 && strcmp(argv[2], "-n") == 0))
 		numeric = 1;
+	if ((argc > 1 && strcmp(argv[1], "-r") == 0) || (argc > 2 && strcmp(argv[2], "-r") == 0))
+		reverse = 1;
+        
 	if ((nlines = readlines(lineptr, MAXLINES)) >= 0) {
-		q_sort((void**)lineptr, 0, nlines-1,
-			(int (*)(void *, void *)) (numeric ? numcmp : strcmp));
+		q_sort((void**) lineptr, 0, nlines-1, 
+			(int (*) (void *, void *)) (
+			(numeric ? (reverse ? reverse_numcmp : numcmp) : strcmp)));
 		writelines(lineptr, nlines);
 		
-		for (int i = 0; i < MAXLINES; i++)
-			if (lineptr[i] == NULL)
-				break;
-			else
-				free(lineptr[i]);	
+		for (int i = 0; i < nlines; i++)
+			free(lineptr[i]);	
 		return 0;
 	} else {
 		printf("input too big to sort\n");	
@@ -39,7 +42,7 @@ int main(int argc, char **argv) {
 
 int readlines(char **restrict lineptr, int maxlines) {
 	int len, nlines;
-	char *p, line[MAXLINES];
+	char *p, line[maxlines];
 
 	nlines = 0;
 	while ((len = get_line(line, maxlines)) > 0)
@@ -48,7 +51,7 @@ int readlines(char **restrict lineptr, int maxlines) {
 		else {
 			line[len - 1] = '\0';
 			strcpy(p, line);
-			lineptr[nlines++] = p;			
+			lineptr[nlines++] = p;
 		}
 
 	return nlines;
@@ -89,6 +92,18 @@ int numcmp(const char *restrict s1, const char *restrict s2) {
 		return 1;
 	else
 		return 0;
+}
+
+int reverse_numcmp(const char *restrict s1, const char *restrict s2) {
+	double v1, v2;
+	
+	v1 = atof(s1);
+	v2 = atof(s2);
+	if (v1 < v2)
+		return 1;
+	else if (v1 > v2)
+		return -1;
+	else return 0;
 }
 
 void swap(void **restrict v, int i, int j) {
