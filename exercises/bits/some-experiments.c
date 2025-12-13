9b789b06c4cadb45220527e4e28562f47dc78854
switchcopa <100treyholds@gmail.com>
2025-12-13 17:13:31 +0100

    add more experiments for endianness

--- exercises/bits/some-experiments.c
+++ exercises/bits/some-experiments.c
@@ -23,6 +23,36 @@ uint32_t reverse_bits(uint32_t x);
 
 int one_bit_set(uint32_t x);
 
+/* Write a function that swaps Endianness for a 32-bit unsigned integer */
+
+void swap_endian(uint32_t *x);
+void swap(uint8_t *a, uint8_t *b); // support
+
+/* Given: You are in a little-endian machine. */
+
+uint8_t buf[8] = {
+	0x00, 0x11, 0x22, 0x33,
+	0x44, 0x55, 0x66, 0x77
+};
+
+/* You do: */
+
+uint32_t *pb = (uint32_t *) buf;
+
+/* What is the hex value of pb[0]? pb[1]? and what happens to the hex value of p[0] when you reverse its endian */ 
+
+/* Answer: pb = (uint32_t *) buf becomes a pointer to a uint32_t, which is 0x33221100, since the MSB in pb[0] is 0x33, and we're in a little-endian
+	machine and the uint8_t's get packed to 4 bytes for each.
+	Therefore pb[0] = 0x33221100
+	and pb[1] = 0x77665544.
+	swap_endian(&pb[0]) -> pb[0] = 0x00112233.
+	We test this in line 72.
+*/	
+
+/* Task: Take 4 bytes from a message uint8_t msg[4], pack them to a uint32_t word, and interpret them as big-endian. 
+	Answer is in line 80.
+*/
+
 int main(void) {
         int x = 0b10101111, y = 0b00001100;
         setbits(&x, 4, 3, y);
@@ -39,6 +69,18 @@ int main(void) {
         p = reverse_bits(p);
         printf("%u, %u\n", p, ~0U);
 	
+	uint32_t n = 0x12345678;
+	swap_endian(&n);
+	printf("%d\n", n == 0x78563412);
+	
+	printf("pb[0] = 0x%X, pb[1] = 0x%X\n", pb[0], pb[1]);
+	swap_endian(&pb[0]);
+	printf("pb[0] = 0x%X\n", pb[0]);
+
+	uint8_t msg[4] = {0x00, 0x11, 0x67, 0x77};
+	uint32_t *ptr = (uint32_t *) msg;
+	printf("packed msg: %s\n", (unsigned char *) ptr);
+
 	return 0;
 }
 
@@ -83,4 +125,16 @@ uint32_t reverse_bits(uint32_t x) {
 
 int one_bit_set(uint32_t x) {
 	return x != 0 && (x & (x - 1)) == 0;
+}
+
+void swap_endian(uint32_t *x) {
+	uint8_t *p = (uint8_t *) x;
+	swap(&p[0], &p[3]);
+	swap(&p[1], &p[2]);
+}
+
+void swap(uint8_t *a, uint8_t *b) {
+	*a = *a ^ *b;
+	*b = *a ^ *b;
+	*a = *a ^ *b;
 }
