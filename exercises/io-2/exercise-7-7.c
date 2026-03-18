712ff821d8dd29fe20eeb0a0fcae1a470d394b83
switchcopa <100treyholds@gmail.com>
2026-03-18 12:53:15 +0000

    WIP: add correct mstrstr function to exercise-7-7 and fix issues

--- exercises/io-2/exercise-7-7.c
+++ exercises/io-2/exercise-7-7.c
@@ -1,49 +1,46 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
 
 #define BUFSIZE 8024
 
 int main(int argc, char **argv) {
     if (argc < 3) {
-        fprintf(stderr, "Usage: ./grep \"pattern\" file1 file2 ...\n")
+        fprintf(stderr, "Usage: ./grep \"pattern\" file1 file2 ...\n");
         exit(EXIT_FAILURE);
     }
 
     const char *pattern = argv[1];
     int nfiles = 2;
 
-    int strstr(const char *dest, const char *pattern);
+    int mstrstr(const char *haystack, const char *needle);
     int getline(char *dest, int max, FILE *fsrc);
 
     char line[BUFSIZE];
     while (nfiles < argc) {
         FILE *fp = fopen(argv[nfiles], "r");
-        if (!fp) { fprintf(stderr, "failed to open %s\n", argv[nfiles--]; continue }
+        if (!fp) { fprintf(stderr, "failed to open %s\n", argv[nfiles++]); continue; }
         while (getline(line, BUFSIZE, fp))
-            if (strstr(line, pattern))
+            if (mstrstr(line, pattern))
                 printf("%s: %s", argv[nfiles], line);
-        nfiles--;
+        nfiles++;
     }
+}
+
+int mstrstr(const char *haystack, const char *needle) {
+    for (int i = 0; haystack[i] != '\0'; i++) {
+        int j, k;
+        for (j = 0, k = i; haystack[k] == needle[j]; k++, j++)
+            ;
+        if (needle[j] == '\0')
+            return 1;
+    }
+
+    return 0;
 }
 
 int getline(char *dest, int max, FILE *fsrc) {
     if (fgets(dest, max, fsrc) == NULL)
         return 0;
     else return strlen(dest);
-}
-
-int strstr(const char *dest, const char *pattern) {
-    int i, j, k;
-    for (i = 0; dest[i] != '\n'; i++)
-        if (dest[i] == pattern[0]) {
-            for (j = 0, k = i; pattern[j] != '\0' && dest[i] == pattern[j]; i++, j++)
-                ;
-            if (pattern[j] == '\0')
-                return 1;
-            else
-                i = k;
-        }
-
-    return 0;
 }
