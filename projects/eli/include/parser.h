1a1a119383f011dbbda4ca1b0bc13adfa3bc425c
switchcopa <100treyholds@gmail.com>
2026-03-26 22:50:47 +0100

    WIP: add parse_expression() function in parser

--- projects/eli/include/parser.h
+++ projects/eli/include/parser.h
@@ -8,6 +8,17 @@
 
 #include "lexer.h"
 #include <stddef.h>
+#include <stdbool.h>
+
+#define EXPECTED_ERROR(c, l) (fprintf(stderr, "Syntax Error: Expected '%c' at line %zu\n", c, l))
+
+typedef enum
+{
+    NODE_NUMBER_DOUBLE,
+    NODE_NUMBER_INT,
+    NODE_BINARY,
+    NODE_UNARY
+} node_type;
 
 typedef struct
 {
@@ -15,10 +26,35 @@ typedef struct
     size_t ntokens;
     size_t capacity;
     size_t pos;
+    size_t line;
     int err;
 } Parser;
 
-void parse_expression(void);
-void expect(toktype);
+typedef struct astnode
+{
+    union
+    {
+        double d;
+        int i;
+
+        struct
+        {
+            struct astnode *right;
+            struct astnode *left;
+            Token t;
+        } unary;
+
+        struct
+        {
+            struct astnode *right;
+            struct astnode *left;
+            Token t;
+        } binary;
+    };
+    node_type type;
+} astnode;
+
+astnode *parse_primary(Parser *p);
+astnode *parse_expression(Parser *, float); // the float is the binding power of each token
 
 #endif
--- projects/eli/src/parser.c
+++ projects/eli/src/parser.c
@@ -2,8 +2,100 @@
 /* parser.c */
 #include "lexer.h"
 #include "parser.h"
+#include <stdlib.h>
+#include <stdbool.h>
 
-void
-parse_expression(void)
+static inline astnode *
+make_binary(astnode *left, Token op, astnode *right)
 {
+    astnode *np = malloc(sizeof(astnode));
+    if (!np) return NULL;
+    np->binary.right = right;
+    np->binary.left = left;
+    np->binry.t = op;
+    return np;
+}
+
+static inline float
+get_precedence(toktype tp)
+{
+    switch (tp)
+    {
+        case TOKEN_PLUS:
+        case TOKEN_MINUS:
+            return 1.0;
+        case TOKEN_STAR:
+        case TOKEN_FSLASH:
+            return 2.0;
+        default:
+            return 0.0;
+    }
+}
+
+static inline Token
+advance(Parser *p)
+{
+    return p->tokens[p->pos++];
+}
+
+static inline Token
+peek(Parser *p)
+{
+    return p->tokens[p->pos];
+}
+
+static inline bool
+match(Parser *p, toktype tp)
+{
+    return (peek(p).type == tp);
+}
+
+astnode *
+parse_primary(Parser *p)
+{
+    astnode *np = malloc(sizeof(astnode));
+    if (!np) return NULL;
+
+    Token t = advance(p);
+    switch (t.type)
+    {
+        case TOKEN_INT:
+            np->type = NODE_NUMBER_INT;
+            np->i = t.i;
+            break;
+        case TOKEN_DOUBLE:
+            np->type = NODE_NUMBER_DOUBLE;
+            np->d = t.d;
+            break;
+        case TOKEN_LPAREN:
+            np = parse_expression(p, 0.0);
+            if (!match(TOKEN_LPAREN))
+                EXPECTED_ERROR(')', p->line);
+            break;
+        default:
+            UNEXPECTED_SYNTAX_ERROR(t.c, p->line);
+            return NULL;
+    }
+
+    return np;
+}
+
+astnode *
+parse_expression(Parser *p, float minbp)
+{
+    astnode *left = parse_primary(p);
+    astnode *right;
+
+    while (true)
+    {
+        Token op = peek(p);
+        float bp = get_precedence(op.type);
+        if (bp < min_bp) break;
+
+        advance();
+        right = parse_expression(p, bp + 0.1);
+        left = make_binary(left, op, right);
+    }
+
+    return left;
 }
