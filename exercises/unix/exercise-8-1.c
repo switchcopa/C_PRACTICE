456e884cae6a34498da0ce281b8941e0e099cf4d
switchcopa <100treyholds@gmail.com>
2026-06-11 10:36:07 +0100

    update exercise 8-1 in K&R chapter 8 cat.c program using UNIX syscalls

--- exercises/unix/exercise-8-1.c
+++ exercises/unix/exercise-8-1.c
@@ -1,52 +1,46 @@
+/* Ex 8-1 cat2.c : concatenate files
+ * using functions from the UNIX 
+ * system calls.
+ */
+
 #include <stdio.h>
-#include <stdarg.h>
 #include <stdlib.h>
-#include <syscalls.h> // This is in my machine's /usr/local/include, 
-                      // you can also find it in /lib/
+#include <stdarg.h>
+#include <syscalls.h>
 
-int main(int argc, char **argv) {
-    int mode = (argc > 1) ? 1 : 0;
-    int nfiles = 1;
-    
-    int mgetchar(int fd);
-    void error(char *fmt, ...);
-    while (!mode || nfiles < argc) {
-        ssize_t n;
-        char buf[BUFSIZ];
-        int fd;
+#define PERMS 0666
 
-        if ((fd = (mode) ? open(argv[nfiles], O_RDONLY, 0) : 0) == -1)
-            error("failed to open file %s", argv[nfiles]);
+void error(char *fmt, ...);
 
-        while ((n = read(fd, buf, sizeof buf)) > 0)
-            if (write(1, buf, n) != n)
-                error("cat: write error");
-        if (!mode) break;
-        nfiles++;
-    }
+int main(int argc, char **argv)
+{
+    char buf[BUFSIZ];
+    int n;
+    int fp;
+
+    if (argc == 1)
+        while ((n = read(0, buf, BUFSIZ)) > 0)
+            write(1, buf, n);
+    else
+        while (--argc > 0)
+        {
+            if ((fp = open(argv[argc], O_RDONLY, 0)) < 0)
+                error("failed to open file %s", argv[argc]);
+            if ((n = read(fp, buf, BUFSIZ)) < 0)
+                error("error occurred while reading file %s", argv[argc]);
+            write(1, buf, n);
+        }
 
     exit(EXIT_SUCCESS);
 }
 
-int mgetchar(int fd) {
-    static char buf[BUFSIZ];
-    static char *bufp;
-    static int n;
-    
-    if (n == 0) {
-        n = read(fd, buf, sizeof buf);
-        bufp = buf;
-    }
-
-    return (--n >= 0) ? (unsigned char) *bufp++ : EOF;
-}
-
-void error(char *fmt, ...) {
+void error(char *fmt, ...)
+{
     va_list args;
 
     va_start(args, fmt);
     fprintf(stderr, "error: ");
-    vprintf(fmt, args);
+    vfprintf(stderr, fmt, args);
     fprintf(stderr, "\n");
     va_end(args);
 
