0f1b31f0aaacef4bd3fa37301e893fa94a6b0d3e
switchcopa <100treyholds@gmail.com>
2026-06-13 13:22:28 +0100

    solve exercise-8-7 improving free and malloc K&R

---
+++ K-R/exercises/unix/exercise-8-7.c
@@ -0,0 +1,70 @@
+/* Exercise 8-7. malloc accepts a size request without
+ * checking its plausibility; free believes that the
+ * block it is asked to free contains a valid size field.
+ * Improve these routines so they make more pains with
+ * error checking.
+ */
+
+#define TOTAL_UNITS 10000U
+
+static Header base;
+static Header *freep = NULL;
+
+/* malloc: general-purpose storage allocator */
+void *malloc(unsigned nbytes)
+{
+    if (nbytes == 0 || nbytes > TOTAL_UNITS)
+        return NULL;
+    Header *p, *prevp;
+    Header *morecore(unsigned);
+    unsigned nunits;
+
+    nunits = (nbytes + sizeof(Header) - 1) / sizeof(Header) + 1;
+    if ((prevp = freep) == NULL) {   /* no free list yet */
+        base.s.ptr = freep = prevp = &base;
+        base.s.size = 0;
+    }
+    for (p = prevp->s.ptr; ; prevp = p, p = p->s.ptr) {
+        if (p->s.size >= nunits) {   /* big enough */
+            if (p->s.size == nunits)  /* exactly */
+                prevp->s.ptr = p->s.ptr;
+            else {                   /* allocate tail end */
+                p->s.size -= nunits;
+                p += p->s.size;
+                p->s.size = nunits;
+            }
+            freep = prevp;
+            return (void *)(p + 1);
+        }
+        if (p == freep)   /* wrapped around free list */
+            if ((p = morecore(nunits)) == NULL)
+                return NULL;  /* none left */
+    }
+}
+
+void free(void *ap)
+{
+    if (ap == NULL)
+        return;
+    Header *np = (Header *)ap - UNIT_SIZE;
+    Header *bp, *p;
+    
+    if (np->s.size <= 1)
+        return;
+    bp = (Header *)ap - 1;    /* point to block header */
+    for (p = freep; !(bp > p && bp < p->s.ptr); p = p->s.ptr)
+        if (p >= p->s.ptr && (bp > p || bp < p->s.ptr))
+            break;    /* freed block at start or end of arena */
+
+    if (bp + bp->s.size == p->s.ptr) {    /* join to upper nbr */
+        bp->s.size += p->s.ptr->s.size;
+        bp->s.ptr = p->s.ptr->s.ptr;
+    } else
+        bp->s.ptr = p->s.ptr;
+    if (p + p->s.size == bp) {            /* join to lower nbr */
+        p->s.size += bp->s.size;
+        p->s.ptr = bp->s.ptr;
+    } else
+        p->s.ptr = bp;
+    freep = p;
+}
